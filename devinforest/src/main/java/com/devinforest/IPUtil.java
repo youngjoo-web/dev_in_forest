d156efe47ee9f84d107fedda98f40ef96ecead5b
gd7 <gd7@DESKTOP-KJD2GAQ>
2020-07-01 16:24:46 +0900

    [이윤선, 최동철] IPUtil 추가
    -com.devinforest.IPUtil 생성
    -FAQController 테스트 코드 작성

---
+++ devinforest/src/main/java/com/devinforest/IPUtil.java
@@ -0,0 +1,20 @@
+package com.devinforest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPUtil {
+	public static String getIPAddress() {
+		// IPv4 방식의 IP주소 가지고 오기
+		InetAddress local = null;
+		try {
+			local = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	   
+		String getIp = local.getHostAddress();
+		System.out.println(getIp + "<-- addQuestion ip");
+		return getIp;
+	}
+}
--- devinforest/src/main/java/com/devinforest/controller/FAQController.java
+++ devinforest/src/main/java/com/devinforest/controller/FAQController.java
@@ -2,10 +2,19 @@ package com.devinforest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.devinforest.IPUtil;
 import com.devinforest.service.FAQService;
 
 @Controller
 public class FAQController {
 	@Autowired private FAQService faqService;
+	
+	@GetMapping("/getFAQList")
+	public String getFAQList() {
+		String getIp = IPUtil.getIPAddress();
+		System.out.println(getIp + " <-- FAQController.getFAQList: getIp");
+		return "FAQ/getFAQList";
+	}
 }
