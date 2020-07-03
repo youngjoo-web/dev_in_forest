787219c5a1353a98454b8f2439d596bc1962ace8
GD1 <GD1@DESKTOP-RCSES4E>
2020-07-03 12:09:29 +0900

    [가재우, 김경태] question 조회 수 기능 수정, question 생성,수정,삭제,백업 question.vo - questionVote생성 question controller - 삭제된질문(백업) 생성, 질문수정 question service - 삭제된 질문(검색,페이징) 생성 modifyQuestion.html 추가

--- devinforest/src/main/java/com/devinforest/service/QuestionService.java
+++ devinforest/src/main/java/com/devinforest/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.devinforest.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,12 +43,13 @@ public class QuestionService {
 		
 		System.out.println(lastPage + "<--  questionServicelastPage");
 		List<Question> questionList = questionMapper.selectQuestionList(map);
-		
+	
 		System.out.println(questionList);
 		
 		Map<String, Object> returnMap = new HashMap<String, Object>();
 		returnMap.put("lastPage", lastPage);
 		returnMap.put("questionList", questionList);
+		
 		return returnMap;
 	}
 	
@@ -64,9 +66,13 @@ public class QuestionService {
 		
 		if(viewsCheck == null) { // member_name == null 이면 조회수를 +1 , 체크를 name으로 할지 ip로 할지는 생각 해봐야 함, 비회원은 어떻게 할 건지도 생각
 			questionMapper.plusViews(question);
+			int viewsCountOne = questionMapper.viewsCountOne(question); // 질문 상세보기의 조회수를 가져온다.
+			
+			// viewsCountOne 변수를 매개변수로 질문 목록의 조회수를 수정해준다. (질문 목록 조회수 = 질문 상세보기 조회수)
+			questionMapper.listViewsCount(viewsCountOne, question.getQuestionNo());
 		}
 		
-		int viewsCount = questionMapper.viewsCount(question);
+		int viewsCount = questionMapper.viewsCountOne(question);
 		Question questionOne = questionMapper.selectQuestionOne(question); 
 			
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -84,7 +90,7 @@ public class QuestionService {
 	
 	
 	public int modifyQuestion(Question question) {
-		return 0;
+		return questionMapper.updateQuestion(question);
 	}
 	
 	/* ---------- 질문 삭제 ---------- */
@@ -98,5 +104,38 @@ public class QuestionService {
 			System.out.println("질문 백업 실패");
 		}
 		return questionMapper.deleteQuestion(question);
+	}
+	/* ---------- 삭제된 질문 목록 ---------- */
+	public Map<String, Object> getRemovedQuestionList(int currentPage, String searchWord) {
+		System.out.println(searchWord + " <-- Service searchWord");
+		
+		
+		int rowPerPage = 5;
+		int beginRow = (currentPage -1) * rowPerPage;
+		int deletedQuestionTotalRow = questionMapper.questionTotalRow(searchWord);
+		System.out.println(deletedQuestionTotalRow + " <-- deletedQuestionTotalRow");
+		int lastPage = deletedQuestionTotalRow / rowPerPage;
+		if(deletedQuestionTotalRow % rowPerPage != 0) {
+			lastPage += 1;
+		}
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchWord", searchWord);
+		map.put("beginRow", beginRow);
+		map.put("rowPerPage", rowPerPage);
+		
+		System.out.println(searchWord + " <--- searchWord");
+		System.out.println(beginRow + " <--- beginRow");
+		System.out.println(rowPerPage + " <--- rowPerPage");
+		
+		System.out.println(lastPage + "<--  questionServicelastPage");
+		List<Question> questionList = questionMapper.selectQuestionList(map);
+	
+		System.out.println(questionList);
+		
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("lastPage", lastPage);
+		returnMap.put("questionList", questionList);
+		
+		return returnMap;
 	}
 }
