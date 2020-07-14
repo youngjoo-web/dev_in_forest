8101e04e96ae350050363a2e1e95dca5b66e882e
gd7 <gd7@DESKTOP-KJD2GAQ>
2020-07-14 14:10:35 +0900

    [최동철] 추가 디자인 변경
    리스트 출력 간격 조정

--- devinforest/src/main/java/com/devinforest/controller/AdminQuestionController.java
+++ devinforest/src/main/java/com/devinforest/controller/AdminQuestionController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.devinforest.service.AdminQuestionService;
-import com.devinforest.vo.LoginAdmin;
 
 @Controller
 public class AdminQuestionController {
