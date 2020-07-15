0bc013ce21c74b1f46f71cfbec73faaf59895e8f
GDJ26 <GDJ26@DESKTOP-BCBMNLO>
2020-07-15 15:07:06 +0900

    [이윤선] 질문조회, 답변조회, 답변댓글조회, 질문댓글조회 스타일 수정 -테이블 너비 조정
    질문 상세보기, 답변상세보기
    -테이블 스타일 수정

--- devinforest/src/main/java/com/devinforest/controller/AdminRestController.java
+++ devinforest/src/main/java/com/devinforest/controller/AdminRestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devinforest.service.AdminQuestionService;
 import com.devinforest.service.AdminService;
-import com.devinforest.vo.Question;
 
 @RestController
 public class AdminRestController {
