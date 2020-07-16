fcb478ea0d6d446ef2ed101b34e68b94080165c5
gd <gd@DESKTOP-5MPQM31>
2020-07-16 20:04:31 +0900

    [백영주] 회원페이지 질문열람기능 버그수정 - questionMapper(XML,interface)일부수정 - DB수정

--- devinforest/src/main/java/com/devinforest/mapper/QuestionMapper.java
+++ devinforest/src/main/java/com/devinforest/mapper/QuestionMapper.java
@@ -9,24 +9,23 @@ import com.devinforest.vo.Question;
 
 @Mapper
 public interface QuestionMapper {
-	public List<Question> selectQuestionList(Map<String, Object> map); // 질문 목록
-	public List<Question> selectQuestionListByHashtag(Map<String, Object> map); //해시태그검색용 질문 목록
-	public int insertQuestion(Question question); // 질문 작성
-	public Question selectQuestionOne(Question question); // 질문 상세보기
-	public int plusViews(Question question); // 조회수 +1 
-	public int viewsCountOne(Question question); // 조회수
-	public String viewsCheck(Question question); // 조회수 체크
-	public int listViewsCount(int viewsCountOne, int questionNo); // 질문리스트 조회수 
-	public int updateQuestion(Question question); // 질문 수정
-	public int deleteQuestion(Question question); // 질문 삭제
-	public int insertQuestionBack(Question question); // 질문 삭제 전 백업
-	public int questionTotalRow(String searchWord); // 질문 총 개수
-	public int deletedQuestionTotalRow(String searchWord); // 삭제된 질문 총 개수
-	public List<Question> selectRomovedQuestion(); // 삭제된 질문 목록
-	public List<Integer> voteTotalCount(List<Integer> questionNo); // 투표 총 카운트
-	public int answersCountOne(Question question); // 질문에 대한 답변 개수
-	public int listAnswersCount(int answersCountOne, int questionNo); // 질문리스트 답변수
-	public int votesCountOne(Question question); // 질문리스트에 대한 투표 개수
-	public int listVotesCount(int votesCountOne, int questionNo); // 질문리스트 투표수
-	public int selectQuestionNoMax(); // 가장 큰 질문번호
+   public List<Question> selectQuestionList(Map<String, Object> map); // 질문 목록
+   public int insertQuestion(Question question); // 질문 작성
+   public Question selectQuestionOne(Question question); // 질문 상세보기
+   public int plusViews(Question question); // 조회수 +1 
+   public int viewsCountOne(Question question); // 조회수
+   public String viewsCheck(Question question); // 조회수 체크
+   public int listViewsCount(int viewsCountOne, int questionNo); // 질문리스트 조회수 
+   public int updateQuestion(Question question); // 질문 수정
+   public int deleteQuestion(Question question); // 질문 삭제
+   public int insertQuestionBack(Question question); // 질문 삭제 전 백업
+   public int questionTotalRow(String searchWord); // 질문 총 개수
+   public int deletedQuestionTotalRow(String searchWord); // 삭제된 질문 총 개수
+   public List<Question> selectRomovedQuestion(); // 삭제된 질문 목록
+   public List<Integer> voteTotalCount(List<Integer> questionNo); // 투표 총 카운트
+   public int answersCountOne(Question question); // 질문에 대한 답변 개수
+   public int listAnswersCount(int answersCountOne, int questionNo); // 질문리스트 답변수
+   public int votesCountOne(Question question); // 질문리스트에 대한 투표 개수
+   public int listVotesCount(int votesCountOne, int questionNo); // 질문리스트 투표수
+   public int selectQuestionNoMax(); // 가장 큰 질문번호
 }
